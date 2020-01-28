7883ca7d8c1b07f750aacdfacd59b7d97ed1a2f5
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-28 15:29:45 +0530

    throw an error

--- program.js
+++ program.js
@@ -22,14 +22,32 @@ const es6 = require('es6-promise');
 // const promise = Promise.resolve('Secret Value');
 // promise.then(console.log).catch(err => console.log(err));
 
-const promiseOne = first()
+// const promiseOne = first()
 
-const promiseTwo = promiseOne.then(function(success){
-  return second(success);
-})
-promiseTwo.then(console.log)
-{
-  function attachTitle(name){
-    return 'DR. ' + name;
-  }
+// const promiseTwo = promiseOne.then(function(success){
+//   return second(success);
+// })
+// promiseTwo.then(console.log)
+
+//   function attachTitle(name){
+//     return 'DR. ' + name;
+//   }
+  
+// Promise.resolve('MANHATTAN').then(attachTitle).then(console.log);
+ 
+
+
+function parsePromised(file) {
+  return new Promise(function (fulfill, reject) {
+    try {
+      fulfill(JSON.parse(file));
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
+function onReject(error) {
+  console.log(error.message);
+}
+parsePromised(process.argv[2])
+.then(null, onReject);

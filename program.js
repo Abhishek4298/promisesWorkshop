8eb31cd38f27f51a02e04ce1abc6c05eb4f70b48
Abhsihek <abhishek.bhavsar@bacancytechnology.com>
2020-01-28 14:52:21 +0530

    values and promises

--- program.js
+++ program.js
@@ -1,11 +1,35 @@
 const es6 = require('es6-promise');
 
-var promise = new Promise(function (fulfill, reject) {
-  fulfill("I FIRED")
-  reject(new Error("I DID NOT FIRE"));
-});
+// var promise = new Promise(function (fulfill, reject) {
+
+//   fulfill("I FIRED")
+//   reject(new Error("I DID NOT FIRE"));
+// });
 //promise.then(console.log)
-function onReject (error) {
-  console.log(error.message);
+
+
+// function onReject (error) {
+//   console.log(error.message);
+// }
+// var promise = new Promise(function(fulfill,reject)
+// {
+//   fulfill("PROMISE VALUE");
+
+// })
+// promise.then(console.log);
+//  console.log("MAIN PROGRAM");
+
+// const promise = Promise.resolve('Secret Value');
+// promise.then(console.log).catch(err => console.log(err));
+
+const promiseOne = first()
+
+const promiseTwo = promiseOne.then(function(success){
+  return second(success);
+})
+promiseTwo.then(console.log)
+{
+  function attachTitle(name){
+    return 'DR. ' + name;
+  }
 }
-promise.then(console.log,onReject);
